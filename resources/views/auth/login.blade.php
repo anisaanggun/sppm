f6d3b211e991e23f4bac24692635ff5938abfc5b
AmbarNingtyas <ambarningtyas321@gmail.com>
2024-10-30 08:52:40 +0700

    revisi login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,12 +7,12 @@
               Login
             </div>
 
-            {{-- @if (session()->has('error'))
+            @if (session()->has('error'))
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
                     {{ session('error') }}
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-            @endif --}}
+            @endif
 
             <div class="card-body">
                 <form method="post" action="/">
