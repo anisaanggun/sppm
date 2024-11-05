d33989801cd700e694fb9ef520b3c9cdefa97a42
AmbarNingtyas <ambarningtyas321@gmail.com>
2024-11-05 10:09:07 +0700

    sidebar

---
+++ resources/views/admin/header.blade.php
@@ -0,0 +1,39 @@
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <!-- SidebarSearch Form -->
+        <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                    <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+      </li>
+
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Navbar Search -->
+      <li class="nav-item">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="/assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Budiman</a>
+            </div>
+        </div>
+      </li>
+
+
+    </ul>
+  </nav>
+  <!-- /.navbar -->
